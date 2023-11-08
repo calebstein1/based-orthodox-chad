c11a1a90af4d5785f43de609a8617c8e3f467e72
Caleb Stein <caleb@calebstein.net>
2023-11-08 01:19:38 -0800

    added page animations

--- shttr/app/views/admin.sh
+++ shttr/app/views/admin.sh
@@ -4,6 +4,7 @@ v shared/app.html
 
 if [ $signed_in -eq 1 ]; then
   vf admin/signed_in.html
+  echo '<turbo-frame id="_queries-list">'
   echo '<ul class="d-flex flex-column gap-2 list-unstyled">'
   for query_id in $queries_id_list; do
     query=$(queries_${query_id} query)
@@ -12,6 +13,7 @@ if [ $signed_in -eq 1 ]; then
     vf admin/query.html
   done
   echo "</ul>"
+  echo "</turbo-frame>"
 else
   vf admin/index.html
 fi
